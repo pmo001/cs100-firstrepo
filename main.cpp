2c073882c26e2faab040600357d6fc47f5f78e7a
Paul Mo <pmo001@hammer.cs.ucr.edu>
2018-01-11 19:13:56 -0800

    added user input

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std
+
+int main()
+{
+	string name;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Hello " << name << "!" << endl;
+
+
+	
+	return 1;
+}
+
